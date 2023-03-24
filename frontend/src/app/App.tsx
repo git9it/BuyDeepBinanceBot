dc604384c6ec455b250a5da310974ceee2182777
git9it <stasdudeyo@gmail.com>
2023-03-24 06:16:10 +0300

    refactoring

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 
 import { LayoutHOC } from '../layout/LayoutHOC';
-import BuyContainer from '../components/BuyContainer/BuyContainer';
-import SellContainer from '../components/SellContainer/SellContainer';
 import HistoryContainer from '../components/HistoryContainer/HistoryContainer';
 import OpenTradesContainer from '../components/OpenTradesContainer/OpenTradesContainer';
-import PairContainer from '../components/PairContainer/PairContainer';
 import CreateTradeContainer from '../components/CreateTradeContainer/CreateTradeContainer';
 
 function App() {
   return (
     <div className="App">
       <CreateTradeContainer />
-
       <HistoryContainer />
       <OpenTradesContainer />
     </div>
