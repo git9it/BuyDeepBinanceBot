a6577e99ae833141ab7a06bb1218269ad5cbbc74
git9it <stasdudeyo@gmail.com>
2023-03-24 06:17:05 +0300

    routes setup

--- server/routes/tradeRoutes.js
+++ server/routes/tradeRoutes.js
@@ -2,11 +2,14 @@ import express from 'express';
 const router = express.Router();
 
 import {
-  createJob,
-  deleteJob,
-  getAllJobs,
-  updateJob,
-  showStats,
+  createNewTrade,
+  getAllTrades,
+  getSingleTrade,
+  deleteTrade,
+  updateTrade,
 } from '../controllers/tradeController.js';
 
-router.route('/').post(createJob).get(getAllJobs);
+router.route('/').post(createNewTrade).get(getAllTrades);
+router.route('/:id').get(getSingleTrade).delete(deleteTrade).patch(updateTrade);
+
+export default router;
