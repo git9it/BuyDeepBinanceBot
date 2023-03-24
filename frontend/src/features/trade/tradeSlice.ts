ba54e8690925b44c1c2f2841c7866b9861893dfa
git9it <stasdudeyo@gmail.com>
2023-03-24 06:16:48 +0300

    redux setup

--- frontend/src/features/trade/tradeSlice.ts
+++ frontend/src/features/trade/tradeSlice.ts
@@ -1,46 +1,44 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const url = 'http://localhost:5000/';
 
-const initialState = {
-  cartItems: [],
-  amount: 4,
-  total: 0,
-  isLoading: true,
-};
+const url = 'http://localhost:5000/api/v1/trades/';
 
-export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+export const getAllTrades = createAsyncThunk('trade/getAllTrades', async () => {
+  console.log('getAllTrades');
   return fetch(url)
     .then((resp) => resp.json())
     .catch((err) => console.log(err));
 });
 
 export const tradeSlice = createSlice({
-  name: '',
+  name: 'trade',
   initialState: {
-    value: 0,
+    allTrades:[],
+    isLoading: true
   },
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
-    },
+    _: (state) => {},
   },
   extraReducers: (builder) => {
-    const { pending, fulfilled, rejected } = getCartItems;
+    const { pending, fulfilled, rejected } = getAllTrades;
 
-    builder.addCase()
-  }
+    builder
+      .addCase(pending, (state) => {
+        //@ts-ignore
+        state.isLoading = true;
+      })
+      .addCase(fulfilled, (state, action) => {
+        console.log(action);
+        //@ts-ignore
+        state.isLoading = false;
+        //@ts-ignore
+        state.allTrades = action.payload;
+      })
+      .addCase(rejected, (state) => {
+        //@ts-ignore
+        state.isLoading = false;
+      });
+  },
 });
 
-export const { reducer: tradeReducer, actions: tradeActions } =
-  tradeSlice;
+export const { reducer: tradeReducer, actions: tradeActions } = tradeSlice;
