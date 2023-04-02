fe24a9768bc48ef303e295624486a05f9bdeb755
git9it <stasdudeyo@gmail.com>
2023-04-02 20:58:32 +0700

    🐛 error catcher for buy function

--- server/api/buySell.js
+++ server/api/buySell.js
@@ -1,17 +1,27 @@
 import client from '../helpers/binance.js';
 import order from '../mocks/buyMock.js';
+import createStore from '../store/createStore.js';
+import actionsReducer from '../store/actionReducer.js';
+import { BUYERROR } from '../store/actions.js';
 
-async function buyAsset(symbol, quantity) {
-  const newTrade = await order({
-    symbol: symbol,
-    side: 'BUY',
-    quantity: quantity,
-    type: 'MARKET',
-  });
- return newTrade
+const store = createStore(actionsReducer);
+
+async function buyAsset(pair, quantity) {
+  try {
+    const newTrade = await client.order({
+      symbol: pair,
+      side: 'BUY',
+      quantity: quantity,
+      type: 'MARKET',
+    });
+    return newTrade;
+  } catch (err) {
+    store.dispatch({ type: BUYERROR, payload: { err, pair } });
+  
+  }
 }
 async function sellAsset(symbol, quantity, price) {
-  return await order({
+  return await client.order({
     symbol: symbol,
     side: 'SELL',
     quantity: quantity,
--- server/store/actionReducer.js
+++ server/store/actionReducer.js
@@ -1,12 +1,14 @@
 import subHelper from '../helpers/subscribe.js';
-import { getPairPriceByInterval } from '../api/fetchData.js';
+import { getPairPriceByInterval, stopFetching } from '../api/fetchData.js';
 import { getCandlesByInterval } from '../api/fetchData.js';
 
 import User from '../models/User.js';
+import BuyTrade from '../models/BuyTrade.js';
 import {
   GETCANDLESBYINTERVAL,
   GETPAIRPRICEBYINTERVAL,
   GETALLUSERS,
+  BUYERROR,
 } from './actions.js';
 
 const sh = subHelper();
@@ -32,6 +34,31 @@ const actionsReducer = async (state = initialState, action) => {
     case GETALLUSERS:
       const allUsers = await User.find({});
       console.log(allUsers);
+      break;
+
+    case BUYERROR:
+      const { pair, err } = action.payload;
+      console.log(err);
+      console.log(`Pair [${pair}] stopped due error: ${err.message}`);
+
+      const trades = await BuyTrade.find();
+      const activeTrades = trades.filter((trade) =>
+        trade.status.includes('Active')
+      );
+
+      const pairActiveTrades = activeTrades.filter((trade) =>
+        trade.pair.includes(pair)
+      );
+      console.log(pairActiveTrades);
+      for (let i = 0; i < pairActiveTrades.length; i++) {
+        console.log(pairActiveTrades[i]._id);
+        await BuyTrade.findByIdAndUpdate(pairActiveTrades[i]._id, {
+          status: 'Error',
+          error: `${err.message}`,
+        });
+      }
+      stopFetching(pair);
+
       break;
 
     default:
